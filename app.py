89e0ab905a7f9ea01eb7227787e93f60b48c493b
Rucha Vaishampayan <37530905+RuchaV@users.noreply.github.com>
2021-11-09 13:44:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="sample")
+# webrtc_streamer(key="sample")
+
+import av
+
+
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        flipped = img[::-1,:,:]
+
+        return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
